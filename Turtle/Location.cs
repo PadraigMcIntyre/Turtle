79036d1cce30fd412379cf71b5f4543d417aa8a7
PadraigMcIntyre <50199854+PadraigMcIntyre@users.noreply.github.com>
2019-06-21 11:17:55 +0100

    My turtle challenge

---
+++ Turtle/Location.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Turtle
+{
+	internal class Location : IEquatable<Location>
+	{
+		// Readonly auto-implemented properties.
+		public int X { get; private set; }
+		public int Y { get; private set; }
+
+		// Set the properties in the constructor.
+		public Location(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return this.Equals(obj as Location);
+		}
+
+		public bool Equals(Location other)
+		{
+			// If parameter is null, return false.
+			if (object.ReferenceEquals(other, null))
+			{
+				return false;
+			}
+
+			// Optimization for a common success case.
+			if (object.ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			// If run-time types are not exactly the same, return false.
+			if (this.GetType() != other.GetType())
+			{
+				return false;
+			}
+
+			// Return true if the fields match.
+			// Note that the base class is not invoked because it is
+			// System.Object, which defines Equals as reference equality.
+			return (X == other.X) && (Y == other.Y);
+		}
+
+		public override int GetHashCode()
+		{
+			return X * 0x00010000 + Y;
+		}
+
+		public static bool operator ==(Location lhs, Location rhs)
+		{
+			// Check for null on left side.
+			if (Object.ReferenceEquals(lhs, null))
+			{
+				if (Object.ReferenceEquals(rhs, null))
+				{
+					// null == null = true.
+					return true;
+				}
+
+				// Only the left side is null.
+				return false;
+			}
+			// Equals handles case of null on right side.
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(Location lhs, Location rhs)
+		{
+			return !(lhs == rhs);
+		}
+	}
+}
---
+++ Turtle/Position.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Turtle
+{
+	internal class Position
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public Direction Direction { get; set; }
+		public int MaxX { get; set; }
+		public int MaxY { get; set; }
+
+		public Position(int x, int y, Direction direction)
+		{
+			X = x;
+			Y = y;
+			Direction = direction;
+		}
+
+		public Position(Position position)
+		{
+			X = position.X;
+			Y = position.Y;
+			Direction = position.Direction;
+			MaxX = position.MaxX;
+			MaxY = position.MaxY;
+		}
+
+		public void Move()
+		{
+			switch (Direction)
+			{
+				case Direction.North:
+					Y--;
+					break;
+				case Direction.East:
+					X++;
+					break;
+				case Direction.South:
+					Y++;
+					break;
+				case Direction.West:
+					X--;
+					break;
+				default:
+					throw new Exception($"Invalid Direction {Direction}");
+			}
+
+			ConfirmPositionIsValid();
+		}
+
+		private void ConfirmPositionIsValid()
+		{
+			if (X < 0 || Y < 0 || X >= MaxX || Y >= MaxY)
+				throw new Exception($"Location ({X}, {Y}) is not valid.");
+		}
+
+		public void Turn()
+		{
+			switch (Direction)
+			{
+				case Direction.North:
+					Direction=Direction.East;
+					break;
+				case Direction.East:
+					Direction = Direction.South;
+					break;
+				case Direction.South:
+					Direction = Direction.West;
+					break;
+				case Direction.West:
+					Direction = Direction.North;
+					break;
+				default:
+					throw new Exception($"Invalid Direction {Direction}");
+			}
+		}
+
+		internal Direction GetDirectionFromText(string facing)
+		{
+			switch (facing)
+			{
+				case "North":
+					return Direction.North;
+				case "East":
+					return Direction.East;
+				case "South":
+					return Direction.South;
+				case "West":
+					return Direction.West;
+				default:
+					throw new Exception("Invalid Facing Direction");
+			}
+		}
+
+		internal Location ToLocation()
+		{
+			return new Location(X, Y);
+		}
+	}
+}
